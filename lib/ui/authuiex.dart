2a0bde52256c95f3bfc52caba670a96578f44d28
safwanjmuhammed <safwanjmuhammed@gmail.com>
2022-11-23 23:48:50 +0530

    fix

--- lib/ui/authuiex.dart
+++ lib/ui/authuiex.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MaterialApp(
