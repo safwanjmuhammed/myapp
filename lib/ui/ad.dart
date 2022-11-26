ec82eeb55ce2461e787d8058332d29e485eb9e47
safwanjmuhammed <safwanjmuhammed@gmail.com>
2022-11-26 18:04:49 +0530

    fix

--- lib/ui/ad.dart
+++ lib/ui/ad.dart
@@ -1,10 +1,11 @@
-import 'dart:html';
+
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: hoteldetails(),
   ));
 }
